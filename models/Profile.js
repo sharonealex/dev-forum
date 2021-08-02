ffa598630ef7d44df39097091885e4431570ca40
Sharon <sharonelizalex@gmail.com>
2021-08-02 19:07:02 +1000

    designed Profile schema

---
+++ models/Profile.js
@@ -0,0 +1,123 @@
+const { Schema, model, Mongoose } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+
+const profileSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+      unique: true,
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String  //ci, junior, archi etc
+    },
+    skills: {
+        type: [String],  //csv in react. convert to obj by JS.
+        required: true
+    },
+    bio:{
+        type: String
+    },
+    githubUser: {
+        type: String
+    },
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String,
+                required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldOfStudy: {
+                type: String,
+                required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    social: {          //object iwth links
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedn: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+    
+   
+});
+  // set this to use virtual below
+
+
+const Profile = model('Profile', profileSchema);
+
+module.exports = Profile;
