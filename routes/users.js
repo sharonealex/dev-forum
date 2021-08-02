36cf5b5ada761e06feffe68ac3a286a5bfe29507
Sharon <sharonelizalex@gmail.com>
2021-08-02 13:01:58 +1000

    jwt token set up for user

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/User');
 const bcryptjs = require('bcryptjs');
 const gravatar = require('gravatar')
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 
 
@@ -12,41 +14,51 @@ router.post('/', async (req, res)=>{
 
     const {name, email, password} = req.body;
     try{
-
-        //workflow
-
     //see if user exists
     let user = await User.findOne({email})
         if(user){
             res.status(400).json({errors: [{msg: 'user already exists'}]})
         }
     //get users gravatar based on their email
-        const avatar = gravatar.url(email, {
-            s: '200',
-            r: 'pg',
-            d: 'mm'
-        })
+    const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+    })
     //encrypt password using bcrypt
     //creates a new user instance
-        user = new User ({
-            name,
-            email,
-            password
-        });
-        const salt = await bcryptjs.genSalt(10);
-        user.password = await bcryptjs.hash(password, salt); //creates a hash.
+    user = new User ({
+        name,
+        email,
+        password
+    });
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(password, salt); //creates a hash.
 
-        //save to db
-        await user.save();
-        res.status(200).send('user reisterd')
+    //save to db
+    await user.save();
+    const payload = {
+        user: {
+            id: user.id
+        }
+    }
+    jwt.sign(
+        payload,
+        config.get('JWT_SECRET'),
+        {
+            expiresIn: 360000
+        },
+        (err, token)=>{
+            if (err) throw err;
+            res.json({token}); 
+        }
+    )
+      
     //return json webtoken, so user is logged in right away without going through another link to log in.
-
     }catch (err){
         console.error(err.message);
         res.status(500).send("internal server error");
     }
-    res.send('User route')
-    
 })
 
 module.exports = router;
