2f0e7ee3a1e76712beb54ff70e0bcf3132426a83
Sharon <sharonelizalex@gmail.com>
2021-08-04 18:21:02 +1000

    reorganized folders

---
+++ server/models/Post.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+      unique: true,
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,//retain post choice if user deletes his account?
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {  //to know which all users are liking it.
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+               type: mongoose.Schema.Types.ObjectId,
+               ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+              }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+ 
+
+
+const Post = mongoose.model('post', postSchema);
+
+module.exports = Post;
---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+  // set this to use virtual below
+
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
