81e8a207a599c1c03ec9c44f7e67a9291f45fa19
Sharon <sharonelizalex@gmail.com>
2021-08-04 13:05:44 +1000

    added Post shcmea

---
+++ models/Post.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+      unique: true,
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,//retain post choice if user deletes his account?
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {  //to know which all users are liking it.
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+               type: Schema.Types.ObjectId,
+               ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+ 
+
+
+const Post = mongoose.model('post', postSchema);
+
+module.exports = Post;
