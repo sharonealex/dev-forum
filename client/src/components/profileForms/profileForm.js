b8633bc9bf9f6f932f5ab3fa60da0d609aa0edb9
Sharon <sharonelizalex@gmail.com>
2021-08-07 22:01:08 +1000

    added event handler for input chnage

--- client/src/components/profileForms/profileForm.js
+++ client/src/components/profileForms/profileForm.js
@@ -38,7 +38,9 @@ const CreateProfile = props => {
     instagram
  } = formData;
 
+ const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
+ const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
 
     return (
         <div>
@@ -52,7 +54,7 @@ const CreateProfile = props => {
       <small>* = required field</small>
       <form className="form" >
         <div className="form-group">
-          <select name="status" value={status} >
+          <select name="status" value={status} onChange = {e => onChange(e)} >
             <option>* Select Professional Status</option>
             <option value="Developer">Developer</option>
             <option value="Junior Developer">Junior Developer</option>
@@ -72,6 +74,7 @@ const CreateProfile = props => {
             type="text"
             placeholder="Company"
             name="company"
+            value={company} onChange = {e => onChange(e)}
           />
           <small className="form-text">
             Could be your own company or one you work for
@@ -82,6 +85,7 @@ const CreateProfile = props => {
             type="text"
             placeholder="Website"
             name="website"
+            value={website} onChange = {e => onChange(e)}
           />
           <small className="form-text">
             Could be your own or a company website
@@ -92,9 +96,10 @@ const CreateProfile = props => {
             type="text"
             placeholder="Location"
             name="location"
+            value={location} onChange = {e => onChange(e)}
           />
           <small className="form-text">
-            City & state suggested (eg. Boston, MA)
+            City & state suggested (eg. Melbourne, MA)
           </small>
         </div>
         <div className="form-group">
@@ -102,16 +107,18 @@ const CreateProfile = props => {
             type="text"
             placeholder="* Skills"
             name="skills"
+            value={skills} onChange = {e => onChange(e)}
           />
           <small className="form-text">
-            Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)
+            Please use comma separated values (eg. Node.js,Graphql)
           </small>
         </div>
         <div className="form-group">
           <input
             type="text"
             placeholder="Github Username"
-            name="githubusername"
+            name="githubUser"
+            value={githubUser} onChange = {e => onChange(e)}
           />
           <small className="form-text">
             If you want your latest repos and a Github link, include your
@@ -122,13 +129,16 @@ const CreateProfile = props => {
           <textarea
             placeholder="A short bio of yourself"
             name="bio"
+            value={bio} onChange = {e => onChange(e)}
           />
           <small className="form-text">Tell us a little about yourself</small>
         </div>
 
         <div className="my-2">
           <button
-           
+           onClick = {()=>{
+             toggleSocialInputs(!displaySocialInputs)
+           }}
             type="button"
             className="btn btn-light"
           >
@@ -137,9 +147,9 @@ const CreateProfile = props => {
           <span>Optional</span>
         </div>
 
-        
-          <div>
-            <div className="form-group social-input">
+        {
+          displaySocialInputs && <div>
+ <div className="form-group social-input">
               <i className="fab fa-twitter fa-2x" />
               <input
                 type="text"
@@ -184,8 +194,9 @@ const CreateProfile = props => {
               />
             </div>
           </div>
+        }
         
-
+      
         <input type="submit" className="btn btn-primary my-1" />
         <Link className="btn btn-light my-1" to="/dashboard">
           Go Back
