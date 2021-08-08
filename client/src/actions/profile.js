b9d39a99ea18b91b9f12a609f7dc274d238d4253
Sharon <sharonelizalex@gmail.com>
2021-08-08 11:45:15 +1000

    updated profile action for delte actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { Router } from "react-router-dom";
 import { setAlert } from "./alert";
 
-import { GET_PROFILE, PROFILE_ERROR } from "./types";
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "./types";
 
 export const getCurrentProfile = () => async (dispatch) => {
   try {
@@ -123,3 +124,40 @@ export const addEducation = (formData, history) => async (dispatch) => {
     });
   }
 };
+
+//delete exp
+
+
+export const deleteExperience = id => async dispatch => {
+ try{
+   const res = await axios.delete(`/api/profile/experience/${id}`);
+   dispatch({
+     type: UPDATE_PROFILE,
+     payload: res.data
+   })
+   dispatch(setAlert("experience removed", "success"));
+ } catch(err){
+   dispatch({
+     type: PROFILE_ERROR,
+     payload: { msg: err.response.statusText, status: err.response.status },
+   })
+ }
+}
+
+//delte edu
+
+export const deleteEducation = id => async dispatch => {
+  try{
+    const res = await axios.delete(`/api/profile/education/${id}`);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    })
+    dispatch(setAlert("education removed", "success"));
+  } catch(err){
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+ }
